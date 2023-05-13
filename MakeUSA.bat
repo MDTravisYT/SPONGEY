af57c620341a32372faf08c5eeb15cbfc4b44e09
MCTravisYT <59547361+MDTravisYT@users.noreply.github.com>
2023-05-13 02:50:58 -0400

    Modification Test: Success!

--- MakeUSA.bat
+++
@@ -1,7 +0,0 @@
-del out\*.* /s /y /e >nul
-@tools\make -r -f makefile.gfx VERSION=DEBUG TERRITORY=USA USER_NAME=CDBUILD
-@tools\make -r -f makefile.gaz VERSION=DEBUG TERRITORY=USA USER_NAME=CDBUILD
-ren cdbuild CdBuild.USA.Debug
-@tools\make -r -f makefile.gfx VERSION=FINAL TERRITORY=USA USER_NAME=CDBUILD
-@tools\make -r -f makefile.gaz VERSION=FINAL TERRITORY=USA USER_NAME=CDBUILD
-ren cdbuild CdBuild.USA.Final
