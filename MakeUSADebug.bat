814d10538d58d9a1e1c1caf95b4eb0f8aa9f4c78
MCTravisYT <59547361+MDTravisYT@users.noreply.github.com>
2023-05-13 12:15:06 -0400

    Brand stuff

--- MakeUSADebug.bat
+++ MakeUSADebug.bat
@@ -2,3 +2,4 @@ del out\*.* /s /y /e >nul
 @tools\make -r -f makefile.gfx VERSION=DEBUG TERRITORY=USA USER_NAME=CDBUILD
 @tools\make -r -f makefile.gaz VERSION=DEBUG TERRITORY=USA USER_NAME=CDBUILD
 ren cdbuild CdBuild.USA.Debug
+pause
